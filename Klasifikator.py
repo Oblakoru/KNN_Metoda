7936159e2ff0015725b7eddea73d544c430f2f81
Benjamin <benjamin.jeran@gmail.com>
2023-11-18 21:10:31 +0100

    razdelitev na 10

--- Klasifikator.py
+++ Klasifikator.py
@@ -1,59 +1,95 @@
 import math
-from sklearn.model_selection import train_test_split
-
 from scipy.spatial.distance import cityblock
 
-
 class Klasifikator():
-
-    #new_instance = [5.0, 3.0, 1.3, 0.2]
-    
-
-    df_ucna_mnozica = None
-    df_testna_mnozica = None
 
     def __init__(self, steviloSosedov, nacinIzracuna="evklidska"):
         self.steviloSosedov = steviloSosedov
         self.nacinIzracuna = nacinIzracuna
+        self.df_ucna_mnozica = None
+        self.df_testna_mnozica = None
 
 
-    def fit(self, vrednostiAtributov):
+    def fit(self, x):
 
-        print("Klasifikator je pripravljen na klasifikacijo.")
-        self.df_ucna_mnozica = vrednostiAtributov
-
-        print(self.df_ucna_mnozica)
+        self.df_ucna_mnozica = x
 
 
     def predict(self, testnaMnozica):
 
         self.df_testna_mnozica = testnaMnozica
 
-        print(testnaMnozica)
-        print(self.df_ucna_mnozica)
+        testnaMnozicaKopija = testnaMnozica.copy()
+
+        for x in [1, 2, 3, 4, 5, 6, 7, 8]:
+
+            oceneList = []
+
+            print(self.df_ucna_mnozica)
+
+
+
+            for rowTest in self.df_testna_mnozica.values:
+
+                razdalje = []
+
+                if self.nacinIzracuna == "evklidska":
+
+                    for row in self.df_ucna_mnozica.values:
+                        distance = math.dist(rowTest[:-1], row[:-1])
+                        razdalje.append(distance)
+                else:
+
+                    for row in self.df_ucna_mnozica.values:
+                        distance = cityblock(row[:-1], rowTest[:-1])
+                        razdalje.append(distance)
+
+                dfRazdalje = self.df_ucna_mnozica.copy()
+                dfRazdalje["Razdalja"] = razdalje
+
+                ocene = dfRazdalje.sort_values(by="Razdalja", inplace=False).head(x)["species"]
+
+                print(f"Klasificiran kot: {ocene.value_counts().idxmax()}, dejansko: {rowTest[-1]}")
+                oceneList.append(ocene.value_counts().idxmax())
+
+            testnaMnozicaKopija[f"Klasifikacija{x}"] = oceneList
+
+        testnaMnozicaKopija.to_csv("data/klasifikacija.csv", index=False)
+
+
+    def predictBasic(self, testnaMnozica):
+
+        self.df_testna_mnozica = testnaMnozica
+        oceneList = []
 
         for rowTest in self.df_testna_mnozica.values:
             razdalje = []
-
             if self.nacinIzracuna == "evklidska":
-
                 for row in self.df_ucna_mnozica.values:
                     distance = math.dist(rowTest[:-1], row[:-1])
                     razdalje.append(distance)
             else:
-
                 for row in self.df_ucna_mnozica.values:
                     distance = cityblock(row[:-1], rowTest[:-1])
                     razdalje.append(distance)
 
+
+            #Naredim kopijo, da se ne spreminja originalni df
             dfRazdalje = self.df_ucna_mnozica.copy()
+
+            #Dodam stolpec z razdaljami
             dfRazdalje["Razdalja"] = razdalje
 
+            #Sortiram po razdalji in vzamem prvih x
             ocene = dfRazdalje.sort_values(by="Razdalja", inplace=False).head(self.steviloSosedov)["species"]
 
             print(f"Klasificiran kot: {ocene.value_counts().idxmax()}, dejansko: {rowTest[-1]}")
 
+            #Dodam v seznam, kjer so vsi rezultati
+            oceneList.append(ocene.value_counts().idxmax())
 
+        testnaMnozica[f"predikcija"] = oceneList
+        testnaMnozica.to_csv("data/klasifikacija.csv", index=False)
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
 from Klasifikator import Klasifikator
 
 #Preberemo podatke ter jih shranimo v df
@@ -7,10 +10,55 @@ df = pd.read_csv('data/IRIS.csv')
 mojKlasifikator = Klasifikator(5, 'evklidska')
 
 #Razdelitev na testno in učno množico
+
+#Od random_state je odvisno, kako se bodo podatki razdelili - ce je isti so zmeraj enako razdeljeni
 train = df.sample(frac=0.8, random_state=200)
+#train.to_csv("data/train.csv", index=False)
+
 test = df.drop(train.index)
+#test.to_csv("data/test.csv", index=False)
 
 mojKlasifikator.fit(train)
 
-mojKlasifikator.predict(test)
+mojKlasifikator.predictBasic(test)
+
+
+
+#####Cross validation
+
+#Premešam podatke v df
+pomesaniPodatki = df.sample(frac=1, random_state=200)
+
+steviloDelov = 10
+
+razdeljeniPodatki = np.array_split(pomesaniPodatki, steviloDelov)
+
+# Display each part
+for i, part in enumerate(razdeljeniPodatki):
+    part.to_csv(f"data/cross_validation/part{i}.csv", index=False)
+
+
+##Testiranje
+def test():
+    df = pd.read_csv('data/klasifikacija.csv')
+
+    y_true = df['species']
+    y_pred = df['predikcija']
+
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f'Accuracy: {accuracy:.2f}')
+
+    conf_matrix = confusion_matrix(y_true, y_pred)
+    print('Confusion Matrix:')
+    print(conf_matrix)
+
+    class_report = classification_report(y_true, y_pred)
+    print('Classification Report:')
+    print(class_report)
+
+
+
+
+
+
 
